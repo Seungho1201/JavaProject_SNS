75d94998bc85d5742d0a5efdfe3af18a925a42e2
Seungho1201 <ohseungho1201@gmail.com>
2025-05-19 19:01:04 +0900

    프로젝트 생성

---
+++ clonestagram/src/test/java/com/example/clonestagram/ClonestagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clonestagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonestagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
